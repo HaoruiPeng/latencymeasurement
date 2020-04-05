00ef4762a863b17fa7410996b080fc010adcb7e1
HaoruiPeng <haorui.peng@eit.lth.se>
2020-04-05 13:25:09 +0200

    add condition to sleep

--- udp-echo/udp-client.py
+++ udp-echo/udp-client.py
@@ -10,7 +10,9 @@ def Send_Handler(s):
         message = "{},{}".format(seq_num, stamp)
         print("send,"+message)
         s.send(message.encode('utf-8'))
-        time.sleep(stamp+0.005-time.time())
+        pause = stamp+0.005-time.time()
+        if pause > 0:
+            time.sleep(pause)
 
 def Recv_Handler(s):
     while True:
@@ -18,10 +20,11 @@ def Recv_Handler(s):
         print("rec,"+str(data.decode('utf-8')))
 
 def Main():
-    host = '127.0.0.1'
+    host = '129.192.69.27'
     port = 12345
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((host,port))
+    print("connect to socket")
     send = threading.Thread(target=Send_Handler, args=(s,))
     recv = threading.Thread(target=Recv_Handler, args=(s,))
     send.start()
